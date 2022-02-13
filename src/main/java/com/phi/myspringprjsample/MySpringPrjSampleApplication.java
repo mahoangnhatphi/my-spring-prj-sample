cd7d8c14fe9c5c96613e133b2333780c4eed5efa
nphi11 <mahoangnhatphi@gmail.com>
2022-02-13 10:53:52 +0700

    Init prj

---
+++ src/main/java/com/phi/myspringprjsample/MySpringPrjSampleApplication.java
@@ -0,0 +1,13 @@
+package com.phi.myspringprjsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringPrjSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringPrjSampleApplication.class, args);
+	}
+
+}
